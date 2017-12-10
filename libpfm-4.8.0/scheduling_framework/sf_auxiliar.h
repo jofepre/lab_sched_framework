218df82d2706ff8e644e0ffa94f1c0daaff94d47
Josué <jofepre@fiv.upv.es>
2017-12-10 23:35:13 +0100

    Project uploaded

---
+++ libpfm-4.8.0/scheduling_framework/sf_auxiliar.h
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2017 Universitat Politecnica de Valencia
+ * Contributed by Josue Feliu <jofepre@gap.upv.es>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#ifndef _SF_AUX_H_
+#define _SF_AUX_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <inttypes.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <sys/wait.h>
+#include <err.h>
+#include <sys/poll.h>
+#include <sys/time.h>
+#include <malloc.h>
+#include <math.h>
+#include <time.h>
+
+#include "perf_util.h"
+
+
+// **
+//
+// DEFINES
+//
+// **
+
+#define MAX_CPUS 4
+#define MAX_EVENTS 6            // Depends on the number of performance counters available
+#define MAX_APPS 24
+#define CPU_FREQ 2700
+
+
+// **
+//
+// STRUCTS
+//
+// **
+
+typedef struct _node {
+    pid_t pid;                  // Proces PID
+    int id;                     // Node identifier (useful when two instances of the same application are launched)
+    int benchmark;              // Application (benchmark identifier)
+    
+    uint64_t target_insts;      // Target number of instructions (set in the node initialization to an offline proffiled value)
+    
+    int status;                 // Used as status variable in the waitpid functions
+    
+    perf_event_desc_t *fds;     // Descriptor to access performance counters
+    
+    int selected;               // Simple flag that determines when a process has already been selected in different parts of the code
+    int cores [MAX_CPUS];       // Cores on which the process will be allowed to run in the next quantum
+    int n_cores;                // Number of valid cores in the previous array
+    
+    uint64_t instructions;      // Overall number of instructions commited
+    uint64_t instructions_q;    // Instructions commieted during the last quantum
+    
+    uint64_t cycles_q;          // Cycles running during the last quantum
+    uint64_t cycles;            // Overall number of cycles running
+    
+    uint64_t events_q [MAX_EVENTS];          // Number of repetitions of event2 during the last quantum
+    uint64_t events [MAX_EVENTS];            // Overall Number of repetitions of event2
+    
+    unsigned long hits_L1;      // L1 hits
+    unsigned long misses_L1;    // L1 misses
+    unsigned long LLC_misses;   // LLC misses
+    
+    int exec_quanta;            // Accounts for the quanta where the process has run
+    int completion_quantum;     // Quantum on which the application reaches its target instructions
+    
+    double BW_L1;               // Bandwidth to access the L1 cache
+    double BW_MM;               // Bandwidth to access main memory
+    
+    double IPC;                 // Saves the IPC of the application once it is completed
+    double quantum_IPC;         // Saves the IPC of the application during the las quantum
+    
+    struct _node * sig;         // Pointer to the next node in the queue
+    struct _node * prv;         // Pointer to the previous node in the queue
+} node;
+
+
+typedef struct _queue {
+    node * head;                // Pointer to the head node in the queue
+    node * tail;                // Pointer to the tail node in the queue (not used)
+    int N;                      // Number of nodes in the queue
+} queue;
+
+
+typedef struct {
+    char *events;
+    int delay;
+    int pinned;
+    int group;
+} options_t;
+
+
+// **
+//
+// VARIABLES
+//
+// **
+
+int quantum;                    // Global quantum counter (accounts the execution quanta)
+
+int num_cores;                  // Input parameter. Number of logical cores considered in the experiment.
+
+int print_per_quantum;          // Determines if the event counts should be printed every quantum
+
+queue process_queue;            // Queue of available processes
+queue running_queue;            // Queue of processes selected to run the next quantum
+queue finished_queue;           // Queue of finished processes
+
+options_t options;              // Options struct variable
+int num_descriptors;            // Number of events being monitored
+
+int *available_cores;           // Array containing the available cores where processes can run (it can be set through an input parameter)
+
+int workload [MAX_APPS];        // Array containing the applications of the workload to be run
+                                // TODO: avoid the maximum number of applications to depend from the MAX_APPS define
+
+char *event_names [MAX_EVENTS]; // Save the name of the events being monitored
+
+struct timeval tvv;             // Auxiliary structures to measure the execution time and scheduling overhead
+struct timezone tzz;
+double start_time_sch, end_time_sch, start_time_aux, end_time_aux;
+double total_scheduling, total_aux;
+
+double BW_L1_medio;
+double BW_MM_medio;
+
+
+// **
+//
+// FUNCTIONS
+//
+// **
+
+// **
+// This function initializes the node structure
+// **
+void initialize_node (node *n, int identificador, int benchmark, unsigned long long int target_insts);
+
+// **
+// This function allocates the memory for a node and initializes it with ident and benchmark
+// **
+node *create_node (int ident, int benchmark, unsigned long long int target_insts);
+
+// **
+// Insert the node n in the queue Q
+// **
+void insert_node (queue *Q, node *n);
+
+// **
+// Pull the node n from the queue Q
+// **
+void pull_node (queue *Q, node *n);
+
+// **
+// This function initializes the queue structure
+// **
+void initialize_queue (queue *Q);
+
+// **
+// This function lists the processes of a queue printing their information
+// Note that is should be edited to make the printed content relevant to the lab session being developed
+// **
+void print_queue (queue *Q);
+
+#endif
+
+
---
+++ libpfm-4.8.0/scheduling_framework/sf_process_selection.c
@@ -0,0 +1,180 @@
+/*
+ * Copyright (c) 2017 Universitat Politecnica de Valencia
+ * Contributed by Josue Feliu <jofepre@gap.upv.es>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+#include "sf_process_selection.h"
+
+// **
+// This function determines which processes will run the next quantum
+// **
+void process_selection (int sel_p) {
+    node *aux, *sig, *max_node;
+    int cpu_remain, n, i;
+    double bw_remain, max_fitness, fit;
+    
+    // **
+    // Select all the processes to be run the next quantum
+    // **
+    if (sel_p == 0) {
+        for (aux = process_queue.head; aux; aux = sig) {
+            pull_node (&process_queue, aux);
+            sig = aux->sig;
+            insert_node (&running_queue, aux);
+        }
+    }
+    
+    
+    // **
+    // Randomly select as many processes as available number of cores
+    // **
+    else if (sel_p == 1) {
+        
+        cpu_remain = num_cores;                 // num_cores refers to logical CPUs (an SMT2 core consists of two logical CPUs for Linux)
+        if (process_queue.N < cpu_remain) {
+            cpu_remain = process_queue.N;
+        }
+        
+        for (cpu_remain; cpu_remain; cpu_remain --) {
+            aux = process_queue.head;
+            n = rand() % process_queue.N;  // random number between 0 and N-1
+            for (i=0; i<n; i++) {
+                aux = aux->sig;
+            }
+            
+            pull_node (&process_queue, aux);
+            insert_node (&running_queue, aux);
+        }
+    }
+    
+    
+    // **
+    // Main memory bandwidth-aware process selection
+    // Selects each quantum as many processes as available number of cores balancing the bandwidth consumption over the workload execution time
+    // The BW_MM variable of the nodes should be correctly updated to use this policy
+    // **
+    else if (sel_p == 2) {
+        
+        fprintf(stderr, "Quantum %d\n", quantum);
+        
+        bw_remain = calculate_avg_MM_BW_per_quantum();          // Set BW remain to the average bandwidth utilization per quantum
+        
+        if (quantum > 5 && bw_remain == 0) {                    // Safety check
+            fprintf(stderr, "Error. BW remain is not being updated.\n");
+            exit (-1);
+        }
+        
+        cpu_remain = num_cores;                         // Set cpu_remain to the number of availabel cpus, unless the number of processes is below this number
+        if (process_queue.N < cpu_remain) {
+            cpu_remain = process_queue.N;
+        }
+        
+        
+        // The first process of the process_queue is always selected to avoid starvation
+        aux = process_queue.head;
+        bw_remain -= aux->BW_MM;
+        cpu_remain --;
+        pull_node (&process_queue, aux);
+        insert_node (&running_queue, aux);
+        
+        
+        // The remaining processes are selected according to their fit to the remaining bandwdith per core
+        while (cpu_remain) {
+            
+            max_fitness = -1;
+            for (aux = process_queue.head; aux; aux = aux->sig) {
+                fit = fitness(bw_remain, aux->BW_MM, cpu_remain);      // We look for the higher fit
+                if (fit > max_fitness) {
+                    max_fitness = fit;
+                    max_node = aux;
+                }
+            }
+            
+            // Insert the selected node in the running_queue and update bw_remain and cpu_remain
+            pull_node (&process_queue, max_node);
+            bw_remain -= max_node->BW_MM;
+            cpu_remain--;
+            insert_node (&running_queue, max_node);
+        }
+    }
+    
+    else {
+        fprintf(stderr, "ERROR!! Process selection policy (%d) not available.\n", sel_p);
+        exit (-1);
+    }
+    
+    //fprintf(stderr, "\nSELECTED PROCESSES: \n");
+    //print_queue (&running_queue);
+}
+
+
+// **
+// This function calculates the fit between predicted and remain per cpu
+// The closer remain is to predicted/cpus the higher the fit
+// **
+double fitness (double remain, double predicted, double cpus) {
+    double aux;
+    
+    aux = (remain/cpus) - predicted;
+    if (aux < 0) {
+        aux = -aux;
+    }
+    return 1/aux;
+}
+
+
+// **
+// This function calculates the average bandwidth utilization per quantum
+// **
+double calculate_avg_MM_BW_per_quantum() {
+    node *aux;
+    double bw = 0;
+    
+    for (aux = process_queue.head; aux; aux = aux->sig) {
+        bw += aux->BW_MM;
+    }
+    
+    return bw  / ((double)process_queue.N / num_cores);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
